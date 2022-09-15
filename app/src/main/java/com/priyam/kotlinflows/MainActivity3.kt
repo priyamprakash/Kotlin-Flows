e0b977d5aeea482d0a0bd4aaa09646622dd4df9e
Priyam Prakash <priyamprakash99@gmail.com>
2022-09-16 01:20:57 +0530

    Cold Flow
    Adding job.cancel in consumer after 3.5 seconds.
    Only first  3 items are produced.

---
+++ app/src/main/java/com/priyam/kotlinflows/MainActivity3.kt
@@ -0,0 +1,38 @@
+package com.priyam.kotlinflows
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+
+class MainActivity3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main3)
+
+        val job = GlobalScope.launch {
+            val data: Flow<Int> = producer()
+            data.collect{
+                Log.d( "onCreate: " , it.toString())
+            }
+        }
+
+        GlobalScope.launch {
+            delay(3500)
+            job.cancel()
+        }
+
+    }
+
+    fun producer() = flow<Int>{
+        val list = listOf(1, 2, 3,4,5,6,7,8,9,10)
+        list.forEach{
+            delay(1000)
+            emit(it)
+        }
+    }
+}
