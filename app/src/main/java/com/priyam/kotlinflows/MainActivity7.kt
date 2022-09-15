e6b6cd07f4aa67ae6db31f481a3b152e60747ca3
Priyam Prakash <priyamprakash99@gmail.com>
2022-09-16 03:17:00 +0530

    Update MainActivity7.kt
    Map, filter and then collect

--- app/src/main/java/com/priyam/kotlinflows/MainActivity7.kt
+++ app/src/main/java/com/priyam/kotlinflows/MainActivity7.kt
@@ -2,10 +2,44 @@ package com.priyam.kotlinflows
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 
+/**collect is terminal operator and is required to start the flow.
+ *
+ * Map and filter are non-terminal
+ */
 class MainActivity7 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main7)
+        GlobalScope.launch(Dispatchers.Main) {
+            producer()
+                .map {
+                    it * 2
+                }
+                .filter {
+                    it < 8
+                }
+                .collect {
+                   Log.d("onCreate: collect", it.toString())
+                }
+
+        }
+
+    }
+
+    private fun producer(): Flow<Int> {
+        return flow<Int> {
+            val list = listOf(1, 2, 3, 4, 5)
+            list.forEach {
+                delay(1000)
+                emit(it)
+            }
+        }
     }
 }
