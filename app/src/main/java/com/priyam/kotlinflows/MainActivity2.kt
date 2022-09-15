c412379a8ed9ddc46ffce1f2a0152cb09067940e
Priyam Prakash <priyamprakash99@gmail.com>
2022-09-16 00:51:57 +0530

    Channel in MainActivity2

---
+++ app/src/main/java/com/priyam/kotlinflows/MainActivity2.kt
@@ -0,0 +1,36 @@
+package com.priyam.kotlinflows
+
+import android.content.ContentValues.TAG
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.launch
+
+class MainActivity2 : AppCompatActivity() {
+
+    val channel = Channel<Int>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        producer()
+        consumer()
+
+    }
+
+    fun producer(){
+        CoroutineScope(Dispatchers.Main).launch {
+            channel.send(1)
+            channel.send(2)
+        }
+    }
+    fun consumer(){
+        CoroutineScope(Dispatchers.Main).launch {
+            Log.d("consumer:" , channel.receive().toString())
+            Log.d("consumer:" , channel.receive().toString())
+        }
+    }
+}
