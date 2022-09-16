78bf30924bc2b00f336be963036d7f640d7be6ec
Priyam Prakash <priyamprakash99@gmail.com>
2022-09-16 22:15:35 +0530

    Update MainActivity11.kt

--- app/src/main/java/com/priyam/kotlinflows/MainActivity11.kt
+++ app/src/main/java/com/priyam/kotlinflows/MainActivity11.kt
@@ -4,11 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 /**
- * Implement try catch - Exception from 4th video after 8th minute of lecture
+ * Implement try catch 
  */
 class MainActivity11 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main11)
+
     }
 }
